9433e9f8267789a26d703752dd1e528d2a81353e
Kriti-kumari221 <kriticse005@gmail.com>
2026-05-18 12:45:02 +0530

    Render deployment support

--- backend/app.py
+++ backend/app.py
@@ -74,4 +74,8 @@ def predict():
 
 if __name__ == "__main__":
     print("\n🎬 CineRead API → http://127.0.0.1:8000\n")
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 8000)),
+    debug=False
+)
